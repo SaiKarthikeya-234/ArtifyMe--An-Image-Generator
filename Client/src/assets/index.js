4b20d044e1e6ab4cbaabcb4aa887d9f868bb452b
SaiKarthikeya-234 <131551309+SaiKarthikeya-234@users.noreply.github.com>
2024-07-16 16:14:42 +0530

    Add files via upload

---
+++ Client/src/assets/index.js
@@ -0,0 +1,18 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import loader from './download.png'
+import like from './like.png'
+import liked from './liked.png'
+
+
+
+
+export {
+  download,
+  logo,
+  preview,
+  loader,
+  like,
+  liked
+};
